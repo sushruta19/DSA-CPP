6ff0b2bb541aac3678f8d27f764871438f91c94d
sushruta19 <sensoubhik2001@gmail.com>
2022-08-08 20:16:55 +0530

    09-count-ksum-subseq.cpp add

---
+++ 5]arrays-strings/09-count-ksum-subseq.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//returning number of subsequences whose sum is k
+//T:O(2^n) AS:O(1)
+int countKsumSubseq(int ind, int sum, vector<int>& arr, int k) {
+  if(ind == arr.size())
+  {
+    if(sum == k)
+      return 1;
+    return 0;
+  }
+  sum+=arr[ind];
+  int l = countKsumSubseq(ind+1,sum,arr,k);
+
+  sum-=arr[ind];
+  int r = countKsumSubseq(ind+1,sum, arr, k);
+
+  return l+r;
+}
+
+int main()
+{
+  vector<int> arr;
+  int n,k; cin>>n>>k;
+  for(int i=0; i<n; i++)
+  {
+    int a; cin>>a;
+    arr.push_back(a);
+  }
+  
+  cout<<"Count: "<<countKsumSubseq(0,0,arr,k);
+}
+
